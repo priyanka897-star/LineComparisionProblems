a892bab314cced24ba7e1bb92919500a385beace
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 15:36:19 +0530

    Added:Functionality to Compare Two Lines

--- javalinecomparisionpractice/LineComparision.java
+++ javalinecomparisionpractice/LineComparision.java
@@ -7,16 +7,18 @@ public class LineComparision {
 		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 		return distance;
 	}
-    public static void main(String[] args) {
+public static void main(String[] args) {
           LineComparision Object = new LineComparision();
           Double line1 = Object.distance(1,2,3,4);
-          Double line2 = Object.distance(1,2,3,4);
-          if (line1.equals(line2)) {
-              System.out.println("Line1 and Line2 are equal");
-          }
-          else {
-        	  System.out.println("Line1 and Line2 are not equal");
-        	  
-           }
-    }
+          Double line2 = Object.distance(1,6,3,4);
+          if(Double.compare(line1,line2) == 0) {
+      		System.out.println("line1 and line2 are equal");
+          }	
+      	else if(Double.compare(line1,line2) < 0) {
+      		System.out.println("line1 is less than line2");
+      	}	
+      	else {
+      		System.out.println("line1 is greater than line2 ");
+      	}
+  }
 }
