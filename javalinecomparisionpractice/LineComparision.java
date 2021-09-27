74ba69716b7bdf73b6e98598c3401a6ebbdc591a
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 15:19:49 +0530

    Added:Code to Calculate Line Length

--- javalinecomparisionpractice/LineComparision.java
+++ javalinecomparisionpractice/LineComparision.java
@@ -1,8 +1,15 @@
 package javalinecomparisionpractice;
 
 public class LineComparision {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparision Program");
-	}
-
+		double distance (int x1,int x2,int y1, int y2)
+		{
+			double distance;
+			distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+			return distance;
+		}
+       public static void main(String[] args) {
+              LineComparision Object = new LineComparision();
+              double computeDistance = Object.distance(1,2,3,4);
+	          System.out.println("Distance between two point is :" +computeDistance);
+   }
 }
