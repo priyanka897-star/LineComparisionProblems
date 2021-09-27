e883c0f76f09535d31f72ad0b1beb13fdb5d2df6
priyanka897-star <priyankamore8997@gmail.com>
2021-09-27 15:28:06 +0530

    Added:code to Check Two Line are Equal or Not

--- javalinecomparisionpractice/LineComparision.java
+++ javalinecomparisionpractice/LineComparision.java
@@ -1,15 +1,22 @@
 package javalinecomparisionpractice;
 
 public class LineComparision {
-		double distance (int x1,int x2,int y1, int y2)
-		{
-			double distance;
-			distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-			return distance;
-		}
-       public static void main(String[] args) {
-              LineComparision Object = new LineComparision();
-              double computeDistance = Object.distance(1,2,3,4);
-	          System.out.println("Distance between two point is :" +computeDistance);
-   }
+	double distance (int x1,int x2,int y1, int y2)
+	{
+		double distance;
+		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return distance;
+	}
+    public static void main(String[] args) {
+          LineComparision Object = new LineComparision();
+          Double line1 = Object.distance(1,2,3,4);
+          Double line2 = Object.distance(1,2,3,4);
+          if (line1.equals(line2)) {
+              System.out.println("Line1 and Line2 are equal");
+          }
+          else {
+        	  System.out.println("Line1 and Line2 are not equal");
+        	  
+           }
+    }
 }
